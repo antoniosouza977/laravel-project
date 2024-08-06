c287e7473b58a36fe22c974ea6cb40dd6fefb6ad
Antonio Souza <129562096+antoniosouza977@users.noreply.github.com>
2024-08-06 14:23:56 -0400

    refactor: install-app.sh

--- docker/scripts/install-app.sh
+++ docker/scripts/install-app.sh
@@ -4,7 +4,6 @@ composer install --no-interaction --no-plugins --no-scripts --prefer-dist
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod 777 -R /var/www/storage
 
-cp /var/www/.env.example /var/www/.env && chmod 777 /var/www/.env
 php artisan migrate
 php artisan db:seed
 php artisan key:generate
